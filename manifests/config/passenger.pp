8784164eb15e5c9ae0d9624f1ba671c68a53d3fb
Stefan Jenkner <stefan@jenkner.org>
2013-03-29 00:25:08 +0100

    Using puppetlabs/apache with apache::mod::passenger.
     - uses apache::vhost with foreman vhost template
     - use_vhost = false ist not yet supported
     - template needs to be splitted for ssl/non-ssl parts
     - consider to use a simpler template for https redirection
       if ssl = true

--- manifests/config/passenger.pp
+++ manifests/config/passenger.pp
@@ -5,8 +5,8 @@ class foreman::config::passenger(
 
 
 ) {
-  include apache::ssl
-  include ::passenger
+  include apache
+  include apache::mod::passenger
 
   # Check the value in case the interface doesn't exist, otherwise listen on all interfaces
   if inline_template("<%= interfaces.split(',').include?(listen_on_interface) %>") == "true" {
@@ -16,17 +16,34 @@ class foreman::config::passenger(
     $listen_interface = '*'
   }
 
-  $foreman_conf = $foreman::use_vhost ? {
-    false   => 'foreman/foreman-apache.conf.erb',
-    default => 'foreman/foreman-vhost.conf.erb',
-  }
+  # define Virtual Host
+  if $foreman::use_vhost {
 
-  file {'foreman_vhost':
-    path    => "${foreman::apache_conf_dir}/foreman.conf",
-    content => template($foreman_conf),
-    mode    => '0644',
-    notify  => Exec['reload-apache'],
-    require => Class['foreman::install'],
+    apache::vhost{ "foreman.${fqdn}" :
+      # Template need to be splitted for SSL/non-SSL - (also consider using a simpler
+      # one for HTTPS redirection: puppetlabs/puppetlabs-apache templates/vhost-redirect.conf.erb)
+      template     => 'foreman/foreman-vhost.conf.erb',
+      # Port will be ignored by the template, but not by the firewall
+      port         => 80,
+      # just ensure that mod_rewrite is loaded
+      redirect_ssl => $foreman::ssl,
+      docroot      => $foreman::app_root,
+    }
+
+    if $foreman::ssl {
+      apache::vhost{ "foreman_ssl.${fqdn}" :
+        # Template need to be splitted for SSL/non-SSL
+        template => 'foreman/foreman-vhost.conf.erb',
+        # Port will be ignored by the template, but not by the firewall
+        port     => 443,
+        # apache::mod:ssl will be included, if ssl = true
+        ssl      => $foreman::ssl,
+        docroot  => $foreman::app_root,
+      }
+    }
+
+  } else {
+    fail "foreman::use_vhost = false is not supported by puppetlabs/apache Module!"
   }
 
   exec {'restart_foreman':
